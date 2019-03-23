a2c0373db446ceaab4c84737e81ae3c0524803b7
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-23 14:20:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def find_the_cheese(items)
     #  items.find {|item| item == cheese_types}
    #end 
      items.find do |item| 
-       item.include?(cheese_types)
+       items.include?(cheese_types)
       
       end 
      
